f3d1622c9096846b8923d97a8e386eb4c9ba2cc8
Arnav Sirigere <arnavsirigere@gmail.com>
2020-10-03 00:14:57 +0530

    Minor refactor :sparkles:

--- content.js
+++ content.js
@@ -5,12 +5,10 @@ checkBonusPoints();
 setInterval(checkBonusPoints, 5000);
 
 function checkBonusPoints() {
-  // Get the div for the button
-  const bonusPointsDiv = document.getElementsByClassName('tw-button tw-button--success tw-interactive');
+  // Get the bonus chest button from where it would usually be
+  const [bonusChest] = document.getElementsByClassName('tw-button tw-button--success tw-interactive');
   // Check if the bonus chest exists, if not we're done here
-  if (!bonusPointsDiv.length) return;
-  // Get the bonus chest from the div
-  const [bonusChest] = bonusPointsDiv;
+  if (!bonusChest) return;
   // Click the bonus chest to claim channel points!
   bonusChest.click();
 }
