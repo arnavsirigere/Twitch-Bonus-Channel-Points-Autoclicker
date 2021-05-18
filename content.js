8e81aa2fa035a57af30f2543d20274161e072d78
Arnav Sirigere <arnavsirigere@gmail.com>
2021-05-18 10:26:16 +0530

    Updating button class name

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ setInterval(checkBonusPoints, 5000);
 
 function checkBonusPoints() {
   // Get the bonus chest button from where it would usually be
-  const [bonusChest] = document.getElementsByClassName('tw-button tw-button--success');
+  const [bonusChest] = document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 VGQNd');
   // Check if the bonus chest exists, if not we're done here
   if (!bonusChest) return;
   // Click the bonus chest to claim channel points!
