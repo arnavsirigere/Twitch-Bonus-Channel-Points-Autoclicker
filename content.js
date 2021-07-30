de4b303453cf0c4571baf945f18cf741db2b3330
Arnav Sirigere <arnavsirigere@gmail.com>
2021-07-30 17:54:12 -0500

    Update class name

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ setInterval(checkBonusPoints, 5000);
 
 function checkBonusPoints() {
   // Get the bonus chest button from where it would usually be
-  const [bonusChest] = document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 VGQNd');
+  const [bonusChest] = document.getElementsByClassName('sc-fzozJi sc-fznKkj jwRWhW');
   // Check if the bonus chest exists, if not we're done here
   if (!bonusChest) return;
   // Click the bonus chest to claim channel points!
